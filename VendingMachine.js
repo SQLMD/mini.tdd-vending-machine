20d1cd451c48b24786d2677e76462da3cc0bd4f7
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-10 12:24:49 +0900

    Pass the initial test for except a coin

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,22 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+class VendingMachine {
+  constructor() {
+    this.till = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+    this.balance = 0;
+  }
+
+  insertCoin(coin) {
+    if (this.till.hasOwnProperty(coin)) {
+      this.till[coin] += 1;
+      this.balance += coin;
+    }
+  }
+}
+module.exports = VendingMachine;
